c508ecb858cde8c2a062e1de27906a310e620a03
GurleenSamrow <gurleenkaur58881@gmail.com>
2023-09-28 13:19:14 +0530

    port

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
+
+
 module.exports.createJwt = async (user) => {
     try {
-        const token = jwt.sign({ data: user }, process.env.SECRETKEY, { expiresIn: '5h' });
+        const token = jwt.sign({ data: user }, process.env.SECRETKEY , { expiresIn: '5h' });
         return token;
     } catch (error) {
         console.log("Error", error);
-        throw error; 
+        throw error;
     }
 };
