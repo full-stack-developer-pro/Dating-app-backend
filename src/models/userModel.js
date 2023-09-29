d8cb7e119e8cf59504ca1ca9b663c502c854bbe1
gurleen <gurleenkaur58881@gmail.com>
2023-09-29 16:36:47 +0530

    age

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -25,6 +25,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['male', 'female','all'], 
     },
+    age: {
+        type : Number,
+    },
     birthdate: {
         type : Date,
     },
