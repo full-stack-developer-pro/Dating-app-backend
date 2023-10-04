1536d2c05401f9b4b796571a666362ec46c33f36
gurleen <gurleenkaur58881@gmail.com>
2023-10-04 13:39:50 +0530

    env

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
-const secret= "mohan@001"
+// const secret= "mohan@001"
 
 module.exports.createJwt = async (user) => {
     try {
-        const token = jwt.sign({ data: user },secret , { expiresIn: '5h' });
+        const token = jwt.sign({ data: user },process.env.SECRETKEY, { expiresIn: '5h' });
         return token;
     } catch (error) {
         console.log("Error", error);
