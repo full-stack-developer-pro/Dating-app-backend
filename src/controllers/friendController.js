6febd6bd63e59395695a910e08b3ce71ce0764eb
gurleen <gurleenkaur58881@gmail.com>
2023-10-30 13:39:30 +0530

    frnds

--- src/controllers/friendController.js
+++ src/controllers/friendController.js
@@ -54,3 +54,42 @@ module.exports.addFriend = async (req, res) => {
     return res.status(500).json(response);
   }
 };
+
+
+
+
+module.exports.removeFriend = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { friendId } = req.body;
+
+    // Check if the friendship exists
+    const existingFriendship = await friendModel.findOne({
+      $or: [
+        { user1: userId, user2: friendId },
+        { user1: friendId, user2: userId },
+      ],
+    });
+
+    if (!existingFriendship) {
+      response.success = false;
+      response.message = 'Friendship does not exist';
+      response.data = null;
+      return res.status(404).json(response);
+    }
+
+    // Remove the friendship
+    await existingFriendship.deleteOne();
+
+    response.success = true;
+    response.message = 'Friend removed successfully';
+    response.data = null;
+    return res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    response.success = false;
+    response.message = 'Internal Server Error';
+    response.data = null;
+    return res.status(500).json(response);
+  }
+};
