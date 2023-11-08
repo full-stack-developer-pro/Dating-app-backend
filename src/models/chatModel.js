692e84369704c39f261d4ef33d155926d2eac3e7
gurleen <gurleenkaur58881@gmail.com>
2023-11-08 14:03:15 +0530

    new changes

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -1,34 +1,34 @@
-// const mongoose = require('mongoose');
-
-// const chatSchema = new mongoose.Schema({
-//   senderId : { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   receiverId : { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   message : {
-//     type : String,
-//     required : true,
-//   },
-// },
-// { timestamps : true },
-// );
-
-// module.exports = mongoose.model('chat', chatSchema);
-
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  messageType: {
-    type: String,
-    enum: ['text', 'wink'],
-    required: true,
-  },
-  message: {
-    type: String,
+  senderId : { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  receiverId : { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  message : {
+    type : String,
+    required : true,
   },
 },
-{
-  timestamps: true,
-});
+{ timestamps : true },
+);
 
 module.exports = mongoose.model('chat', chatSchema);
+
+// const mongoose = require('mongoose');
+
+// const chatSchema = new mongoose.Schema({
+//   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   messageType: {
+//     type: String,
+//     enum: ['text', 'wink'],
+//     required: true,
+//   },
+//   message: {
+//     type: String,
+//   },
+// },
+// {
+//   timestamps: true,
+// });
+
+// module.exports = mongoose.model('chat', chatSchema);
