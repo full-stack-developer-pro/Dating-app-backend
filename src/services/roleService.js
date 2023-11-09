4c4a6abca5930c287b22ca15caa70d84b0987d2e
GurleenSamrow <gurleenkaur58881@gmail.com>
2023-11-09 12:39:25 +0530

    approve

--- src/services/roleService.js
+++ src/services/roleService.js
@@ -12,7 +12,10 @@ module.exports.admin= async (req,res,next) => {
   
 module.exports.user= async (req,res,next) => {
   try {
-    if(req.info.role === '2' || req.info.role === '1'){
+
+
+    if(req.info.role === '2'  || req.info.role === '1'){
+
        next()
     }else{
       res.status(401).send({success:false,message:"Invalid Request"})
